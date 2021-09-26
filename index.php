19d5a7e087125a971564b4e46e8bb4dbf6d48784
Brendon Bown <brendon.bown@gmail.com>
2021-09-26 00:03:21 -0600

    Final product

--- index.php
+++ index.php
@@ -13,32 +13,29 @@ $products['Car'] = 15000;
 $products['iPhone'] = 1000;
 $products['Toaster'] = 75;
 
+$taxRate = 0.0825;
+
 echo "<h1>Welcome to ".$name."!</h1>";
 echo "<h2>You have $".$credit." in your wallet.</h2>";
 
 foreach($products as $key => $value) {
-    echo "<p>The ".$key." costs ".$value."</p>";
+    $costWithTax = tax_calc($value, $taxRate);
+    echo "<p>The ".$key." costs ".$costWithTax." with tax</p>";
 }
 
 echo "<h2>Items you can afford</h2>";
 foreach($products as $key => $value) {
-    if($value <= $credit) {
+    $costWithTax = tax_calc($value, $taxRate);
+    if($costWithTax <= $credit) {
         echo "<p>".$key."</p>";
     }
 }
-
-$amount = 800;
-$taxRate = 0.0825;
-$addedTax = $amount * $taxRate;
-echo $addedTax;
 
 function tax_calc($amount, $tax) {
     $calculate_tax = $amount * $tax;
     $amount = round($amount + $calculate_tax, 2);
     return $amount;
 }
-
-echo "<p>".tax_calc(750, 0.223)."</p>";
     ?>
   </body>
 </html>
